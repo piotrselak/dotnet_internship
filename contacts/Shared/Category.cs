5ed6b71cd4045eb8f32adf5e6c021c646e8638f7
piotrselak <piotr.selak.it@gmail.com>
2023-12-07 00:44:09 +0100

    Fixed domain

--- contacts/Shared/Category.cs
+++ contacts/Shared/Category.cs
@@ -4,5 +4,5 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public SubCategory? SubCategory { get; set; } // TODO rethink it - it's not the right way
+    public ICollection<SubCategory>? SubCategory { get; set; }
 }
--- contacts/Shared/Contact.cs
+++ contacts/Shared/Contact.cs
@@ -1,13 +1,17 @@
-﻿namespace contacts.Shared;
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace contacts.Shared;
+
+[Index(nameof(Email), IsUnique = true)]
 public class Contact
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Email { get; set; } // TODO make unique in db?
+    public string Email { get; set; }
     public string Password { get; set; }
     public string PhoneNumber { get; set; }
     public DateOnly BirthDate { get; set; }
     public Category Category { get; set; }
+    public SubCategory? SubCategory { get; set; }
 }
