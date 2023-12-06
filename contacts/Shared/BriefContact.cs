662f15f521634f869c6cdbfb483a6a499fec4a6d
piotrselak <piotr.selak.it@gmail.com>
2023-12-06 19:26:27 +0100

    Added map helper

--- contacts/Shared/BriefContact.cs
+++ contacts/Shared/BriefContact.cs
@@ -6,4 +6,14 @@ public class BriefContact
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+    
+    // Helpful when mapping Contacts from db to brief versions
+    public static BriefContact FromContact(Contact contact) 
+        => new BriefContact
+            {
+                Id = contact.Id,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+                Email = contact.Email
+            };
 }
