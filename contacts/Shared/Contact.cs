0f53c8e0a00430a163f55526cf04da3c98c66378
piotrselak <piotr.selak.it@gmail.com>
2023-12-06 19:09:51 +0100

    Added BriefContact dto

--- contacts/Shared/Contact.cs
+++ contacts/Shared/Contact.cs
@@ -5,7 +5,7 @@ public class Contact
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Email { get; set; }
+    public string Email { get; set; } // TODO make unique in db?
     public string Password { get; set; }
     public string PhoneNumber { get; set; }
     public DateOnly BirthDate { get; set; }
